e5842d3016dc25a1414f899b6dfb5f8b5c0d7a92
Nandhidha118 <70620011+Nandhidha118@users.noreply.github.com>
2023-07-23 18:48:18 +0530

    Add files via upload

---
+++ priority queue.c
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int find_kth_smallest_element(priority_queue<int,vector<int>, greater<int>>& pq, int k) {
+
+    for (int i = 1; i < k; ++i) {
+        pq.pop();
+    }
+    return pq.top();
+}
+
+int main() {
+    priority_queue<int, vector<int>,greater<int>> priority_queue;
+
+    priority_queue.push(3);
+    priority_queue.push(2);
+    priority_queue.push(4);
+    priority_queue.push(1);
+    priority_queue.push(5);
+    priority_queue.push(6);
+
+    int k ;
+    cout<<"enter a value:";
+    cin>>k;
+    int result = find_kth_smallest_element(priority_queue, k);
+      cout << "The " << k << "th smallest element is: " << result << endl;
+
+    return 0;
+}
